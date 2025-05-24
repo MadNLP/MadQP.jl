77f55d7a55bf4c8b465498f5f336f594b9bc7ce9
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-05-24 01:20:53 -0500

    Add a kernel for ScaledSparseKKTSystem

--- scripts/cuda_wrapper.jl
+++ scripts/cuda_wrapper.jl
@@ -85,3 +85,32 @@ function LinearAlgebra.mul!(y::CuVector{T}, A::MadQPOperator{T}, x::CuVector{T})
     beta = Ref{T}(zero(T))
     CUSPARSE.cusparseSpMV(CUSPARSE.handle(), A.transa, alpha, A.descA, descX, beta, descY, T, algo, A.buffer)
 end
+
+# Should be backported in MadNLPGPU.jl in the future
+@kernel function _scale_augmented_system_coo_kernel!(dest_V, @Const(src_I), @Const(src_J), @Const(src_V), @Const(scaling), @Const(n), @Const(m))
+    k = @index(Global, Linear)
+    i = src_I[k]
+    j = src_J[k]
+
+    # Primal regularization pr_diag
+    if k <= n
+        dest_V[k] = src_V[k]
+    # Hessian block
+    elseif i <= n && j <= n
+        dest_V[k] = src_V[k] * scaling[i] * scaling[j]
+    # Jacobian block
+    elseif n + 1 <= i <= n + m && j <= n
+        dest_V[k] = src_V[k] * scaling[j]
+    # Dual regularization du_diag
+    elseif (n + 1 <= i <= n + m) && (n + 1 <= j <= n + m)
+        dest_V[k] = src_V[k]
+    end
+    nothing
+end
+
+function MadNLP._build_scale_augmented_system_coo!(dest, src, scaling::CuArray, n, m)
+    backend = CUDABackend()
+    kernel! = _scale_augmented_system_coo_kernel!(backend)
+    N = nnz(src)
+    kernel!(dest.V, src.I, src.J, src.V, scaling, n, m; ndrange = N)
+end
